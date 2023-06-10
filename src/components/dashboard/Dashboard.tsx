5cdeee98085a09505b9dd534d734000fbe2591ac
Masaya Shida <masayashida@gmail.com>
2023-06-10 16:13:14 +0700

    fix dashboard td padding

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -104,7 +104,7 @@ const Dashboard: FunctionComponent = () => {
                   />
                 })
                 : <tr>
-                  <td>
+                  <td style={{padding: ".75rem .5rem"}}>
                     <span>មិនទាន់មានអត្តបទទេ។</span>
                     <Link to="/editor/new">
                       <span>&nbsp;បង្កើតអត្តបទថ្មី&nbsp;→</span>
